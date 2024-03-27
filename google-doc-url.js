f6a502e0e2284dda334b0d442bf1782628b3427d
Trevor Anderson <saqikawaay@gmail.com>
2024-03-27 20:15:26 +1000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1qld18MKy4RrPB0DjBkDC4v8ET3iwzQOg7tnN0Po95q4/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = '';
+var googleApiKey = 'AIzaSyAlpalrC-A1sMynV5yUn3TPAinDGuZhT1g';
